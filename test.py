ffef50f012ec8836cd02f93b20321f4aef3dd34c
HV-ER30 <81569221+HV-ER30@users.noreply.github.com>
2022-04-02 19:13:56 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+Created on 
+
+Course work: 
+
+@author: harshavardhan
+
+Source:
+    
+'''
+# Import necessary modules
+
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+
+    return render_template('index.html')
+
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", debug = True, port = 5003)
